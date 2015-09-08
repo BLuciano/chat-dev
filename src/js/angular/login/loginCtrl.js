a991c8c062c01a8eb62c9721b20ac7d7f33e2c99
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2015-09-07 17:41:56 -0700

    Usernames are now saved to database on registration

--- src/js/angular/login/loginCtrl.js
+++ src/js/angular/login/loginCtrl.js
@@ -1,26 +1,41 @@
 "use strict";
 
-angular.module('chatApp').controller("loginCtrl", ['$scope', '$firebaseAuth', 
-	function($scope, $firebaseAuth){
-		//set link reference to user's database 
-		getLoginRef = function(){
-			var ref = new Firebase("https://vivid-inferno-5718.firebaseio.com");
-			return $firebaseAuth(ref);
+angular.module('chatApp').controller("loginCtrl", ['$scope', '$firebaseAuth', '$firebaseArray',
+	function($scope, $firebaseAuth, $firebaseArray){
+		//when a new user is created, save the username reference in the
+		//database for later use while chatting.  
+		saveUser = function(){
+			var ref =  new Firebase("https://vivid-inferno-5718.firebaseio.com/Users");
+			$firebaseArray(ref).$add({
+				userName: $scope.newUserName.toLowerCase()
+			});
 		};
+
+		//Clear the registration form fields once the form is successfully submitted.
+		clearFields = function(){
+			$scope.newEmail = "";
+			$scope.newPassword = "";
+			$scope.passRepeat = "";
+			$scope.newUserName = "";
+		}
 		
+		//create a new user 
 		$scope.createUser = function(){
 			$scope.message = null;
 			$scope.error = null;
+			var loginRef =  new Firebase("https://vivid-inferno-5718.firebaseio.com");
 			
+			//First make sure passwords are matching
 			if($scope.newPassword !== $scope.passRepeat){
 				$scope.error = "Passwords must match";
 			} else {
-				var ref = getLoginRef();
-				ref.$createUser({
+				$firebaseAuth(loginRef).$createUser({
 					email: $scope.newEmail,
 					password: $scope.newPassword
 				}).then(function(userData) {
 					$scope.message = "User successfully created!";
+					saveUser();   //call saveUser once the form successfully passes.
+					clearFields();
 				}).catch(function(error){
 					if(error === "undefined"){
 						$scope.error = "Unknown error trying to create user";
@@ -29,6 +44,6 @@ angular.module('chatApp').controller("loginCtrl", ['$scope', '$firebaseAuth',
 					}
 				});
 			}
-		};
+		}; //end of createUser
 	}
 ]);
