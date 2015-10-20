b32622a382685d28473bcc487ab3339c818dadab
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2015-10-20 03:05:52 -0700

    Changed layout and styles

--- src/js/angular/app.js
+++ src/js/angular/app.js
@@ -39,6 +39,10 @@
 	.config(['$routeProvider', function($routeProvider){
 		$routeProvider
 		.when('/', {
+			templateUrl: 'views/landing.html',
+			controller: 'loginCtrl'
+		})
+		.when('/login', {
 			templateUrl: 'views/login.html',
 			controller: 'loginCtrl'
 		})
